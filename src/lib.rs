f7e64baf857155b42530f921568a5749d98e03ce
MordechaiHadad <mordechai.hadad01@gmail.com>
2022-10-24 14:45:13 +0300

    added: binary search, and 2 crystal balls NICEO

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,47 @@ mod search;
 
 #[cfg(test)]
 mod tests {
-    use crate::search::linear_search;
+    use crate::search::*;
 
     #[test]
     fn linear_search_test() {
         let result = &[1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
         assert_eq!(linear_search(result, 69), true);
-        assert_eq!(linear_search(result, 1336), false);
+        assert_eq!(linear_search(result, 1336), false); // rust doesnt support test cases
+                                                        // unfortunate
         assert_eq!(linear_search(result, 69420), true);
         assert_eq!(linear_search(result, 69421), false);
         assert_eq!(linear_search(result, 1), true);
         assert_eq!(linear_search(result, 0), false);
+    }
+
+    #[test]
+    fn binary_search_test() {
+        let result = &[1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
+        assert_eq!(binary_search(result, 69), true);
+        assert_eq!(binary_search(result, 1336), false);
+        assert_eq!(binary_search(result, 69420), true);
+        assert_eq!(binary_search(result, 69421), false);
+        assert_eq!(binary_search(result, 1), true);
+        assert_eq!(binary_search(result, 0), false);
+    }
+
+    #[test]
+    fn two_crystal_balls_test_equal() {
+        let idx = (rand::random::<f64>() * (10000 as f64)) as i32;
+        let mut data = [false; 10000];
+
+        let mut i = idx;
+        while i < 10000 {
+            data[i as usize] = true;
+            i += 1;
+        }
+
+        assert_eq!(two_crystal_balls(&data), idx);
+    }
+
+    #[test]
+    fn two_crystal_balls_test_negative() {
+        assert_eq!(two_crystal_balls(&[false; 821]), -1);
     }
 }
--- src/search.rs
+++ src/search.rs
@@ -1,9 +1,52 @@
 pub fn linear_search(haystack: &[i32], needle: i32) -> bool {
     for n in 0..haystack.len() {
-
         if haystack[n] == needle {
             return true;
         }
     }
     false
+}
+
+pub fn binary_search(haystack: &[i32], needle: i32) -> bool {
+    let mut lo = 0;
+    let mut hi = haystack.len();
+
+    while lo < hi {
+        let m: usize = lo + (hi - lo) / 2;
+        let value = haystack[m];
+
+        if value == needle {
+            return true;
+        } else if value > needle {
+            hi = m;
+        } else {
+            lo = m + 1;
+        }
+    }
+    false
+}
+
+pub fn two_crystal_balls(breaks: &[bool]) -> i32 {
+    let jump_amount = f32::sqrt(breaks.len() as f32) as i32;
+    let mut i = jump_amount;
+
+    while i < breaks.len() as i32 {
+        if breaks[i as usize] {
+            break;
+        }
+        i += jump_amount;
+    }
+    i -= jump_amount;
+
+    let mut j = 0;
+    while j < jump_amount && i < breaks.len() as i32 {
+        if breaks[i as usize] {
+            return i;
+        }
+
+        i += 1;
+        j += 1;
+
+    }
+    -1
 }
